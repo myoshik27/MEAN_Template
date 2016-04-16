b6d87da18cb71f9ee86b6eb8122e1dece6b3b47a
Matt Yoshihara <mkyoshihara@gmail.com>
2016-04-15 17:44:57 -0700

    added header comments

---
+++ server/config/routes.js
@@ -0,0 +1,9 @@
+// this is our routes.js file located at /server/config/routes.js
+// this file processes get and post requests from the factories
+// routes will then call on the server controller functions
+
+// this file will be called on by the main server.js file and it will be passed the app variable. 
+
+// the routes.js file must require all of the server controllers("the exact route from the base file") in order to call upon their functions
+
+
