6d1f6a3bb82d87acba78beee8261bdf359d84d37
Matt Yoshihara <mkyoshihara@gmail.com>
2016-04-15 22:52:34 -0700

    added header comments

---
+++ client/static/js/factories/userAngFac.js
@@ -0,0 +1,14 @@
+// factory methods get called by the controller and call on routes. On success, factory should run controller supplied callback function
+
+/*
+
+app_name_here.factory("FactoryNameHere", function($http){
+	var factory = {
+
+	// Factory Methods go here
+
+	}
+	
+})
+
+*/
