202f7a5e9a9e5a1621064e32f516266c785e9222
Matt Yoshihara <mkyoshihara@gmail.com>
2016-04-15 18:02:36 -0700

    added example model code

---
+++ server/models/userServerModel.js
@@ -0,0 +1,13 @@
+// this is the userServerModel.js file located at server/models/userServerModel.js
+// this file is to create the schema users in our mongodb
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UsersSchema = new mongoose.Schema({
+	name: {type: String, required: true},
+	created_at: {type: Date, default: Date.now, required: true},
+	orders: [{type: Schema.Types.ObjectId, ref: "Orders"}]
+})
+
+mongoose.model("Users", UsersSchema);
