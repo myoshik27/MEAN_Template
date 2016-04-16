cc076b503c3eb367e140979dd471448af915001b
Matt Yoshihara <mkyoshihara@gmail.com>
2016-04-15 22:50:40 -0700

    added header comments

--- client/static/js/controllers/usersAngCtrl.js
+++ client/static/js/controllers/usersAngCtrl.js
@@ -1,3 +1,6 @@
+// Controller methods get called by partials and call on factory methods
+// Controller anonymous function may inject multiple factories if needed
+
 /*
 
 app_name_here.controller("ControllerNameHere", function(CorrespondingFactoryNameHere){
